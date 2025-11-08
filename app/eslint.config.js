7e823c20e78c4da5292f63ecfd6cded404a9125a
Tarang74 <31427635+Tarang74@users.noreply.github.com>
2025-11-09 01:07:58 +1000

    Fix lint errors and add new favicon.

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -2,10 +2,13 @@ import eslint from "@eslint/js";
 import globals from "globals";
 import prettierPlugin from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
+import astroPlugin from "eslint-plugin-astro";
 
 export default [
+  eslint.configs.recommended,
+  ...astroPlugin.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts,tsx,astro}"],
+    files: ["**/*.{js,mjs,ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -15,12 +18,17 @@ export default [
       prettier: prettierPlugin,
     },
     rules: {
-      ...eslint.configs.recommended.rules,
       ...prettierConfig.rules,
       "prettier/prettier": "error",
     },
   },
   {
-    ignores: ["dist/**", "node_modules/**", ".astro/**"],
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      ".astro/**",
+      "**/*.scss",
+      "**/*.css",
+    ],
   },
 ];
